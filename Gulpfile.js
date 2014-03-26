be5d3bf14781ae75daaf1fd4a05954eb8bebe78e
Eliseu Monar <eliseumds@gmail.com>
2014-03-25 23:51:28 -0300

    adding express server and React server-side rendering

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,25 +6,18 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var plumber = require('gulp-plumber');
 
-var connect = require('gulp-connect');
 var react = require('gulp-react');
 var watch = require('gulp-watch');
 
-gulp.task('connect', connect.server({
-    root: ['app'],
-    port: 8000,
-    livereload: true
-}));
-
 gulp.task('jsx', function() {
-    gulp.src('./app/components/**/*.jsx')
+    gulp.src('./public/js/components/**/*.jsx')
         .pipe(watch(function(files) {
             return files.pipe(plumber())
                         .pipe(react())
                         .on('error', gutil.log)
                         .on('error', gutil.beep)
-                        .pipe(gulp.dest('./public/js/app/components'));
+                        .pipe(gulp.dest('./public/js/components'));
         }));
 });
 
-gulp.task('default', ['connect', 'jsx']);
+gulp.task('default', ['jsx']);
