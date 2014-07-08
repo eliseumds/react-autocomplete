a8fdb7092446e84a5637df3de96a5cced6fe3f2e
aymanosman <aymano.osman@gmail.com>
2014-07-08 10:42:58 +0100

    Fix link to rxjs dependency

--- public/js/init.js
+++ public/js/init.js
@@ -6,7 +6,7 @@ requirejs.config({
     paths: {
         'lodash': '../vendor/lodash/dist/lodash.min',
         'react/addons': '../vendor/react/react-with-addons',
-        'rx': '../vendor/rxjs/rx.lite',
+        'rx': '../vendor/rxjs/dist/rx.lite',
     },
     urlArgs: 'bust=' + (+new Date())
 });
